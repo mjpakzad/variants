183fb34f3d1327a02d4a1c53a49ac95ea5222e12
Mj Pakzad <mj.pakzad@gmail.com>
2022-09-20 02:13:14 +0430

    Move variants and orders array to config files

---
+++ configs/orders.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'title'     => 'asc',
+    'seller_id' => 'desc'
+];
---
+++ configs/variants.php
@@ -0,0 +1,59 @@
+<?php
+return [
+    [
+        'id'        => 1,
+        'title'     => 'A1',
+        'seller_id' => 1,
+        'price'     => 15000,
+        'rate'      => 4.5,
+    ],
+    [
+        'id'        => 2,
+        'title'     => 'A2',
+        'seller_id' => 1,
+        'price'     => 20000,
+        'rate'      => 3.2,
+    ],
+    [
+        'id'        => 3,
+        'title'     => 'A3',
+        'seller_id' => 2,
+        'price'     => 68000,
+        'rate'      => 4.8,
+    ],
+    [
+        'id'        => 4,
+        'title'     => 'A1',
+        'seller_id' => 3,
+        'price'     => 14500,
+        'rate'      => 4.4,
+    ],
+    [
+        'id'        => 5,
+        'title'     => 'A1',
+        'seller_id' => 4,
+        'price'     => 15100,
+        'rate'      => 5,
+    ],
+    [
+        'id'        => 6,
+        'title'     => 'A2',
+        'seller_id' => 3,
+        'price'     => 18000,
+        'rate'      => 4.7,
+    ],
+    [
+        'id'        => 7,
+        'title'     => 'A3',
+        'seller_id' => 4,
+        'price'     => 68500,
+        'rate'      => 4.9,
+    ],
+    [
+        'id'        => 8,
+        'title'     => 'A1',
+        'seller_id' => 2,
+        'price'     => 15500,
+        'rate'      => 5,
+    ],
+];
